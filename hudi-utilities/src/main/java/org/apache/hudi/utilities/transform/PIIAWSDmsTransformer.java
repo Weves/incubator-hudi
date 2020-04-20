c446e8310bf04dc45434e4566736397c39dc3f13
Chris Weaver <chris.weaver@robinhood.com>
2020-04-20 15:47:43 -0700

    Initial transformer to remove PII columns. Takes in environment variables + config files and uses those to determine which columns need to be dropped.

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/transform/PIIAWSDmsTransformer.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/transform/PIIAWSDmsTransformer.java
@@ -28,10 +28,14 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.HashSet;
+import java.io.FileReader;
 
-// import static org.apache.spark.sql.functions.when;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import static org.apache.spark.sql.functions.lit;
-// import static org.apache.spark.sql.functions.col;
 
 /**
  * A Simple transformer that adds `Op` field with value `I`, for AWS DMS data, if the field is not
@@ -39,19 +43,107 @@ import static org.apache.spark.sql.functions.lit;
  */
 public class PIIAWSDmsTransformer implements Transformer {
 
+  private static final String DB_NAME_ENV = "DATABASE_NAME";
+  private static final String TABLE_NAME_ENV = "TABLE_NAME";
+  private static final String BLACKLIST_ENTRY = "COMMON_PII_COLS";
+  private static final String WHITELIST_CONFIG_PATH = "/Users/chris.weaver/Downloads/whitelist.json";
+  private static final String BLACKLIST_CONFIG_PATH = "/Users/chris.weaver/Downloads/blacklist.json";
+
+  // private static final String[] HUDI_COLUMNS = {"_hoodie_commit_time", "_hoodie_commit_seqno",
+  //                                               "_hoodie_record_key", "_hoodie_partition_path",
+  //                                               "_hoodie_file_name", "update_timestamp"};
+  private static final String[] HUDI_COLUMNS = {"update_timestamp", "Op"};
+
+  private HashSet<String> whitelistColumnNames;
+  private HashSet<String> blacklistColumnNames;
+  private String databaseName;
+  private String tableName;
+  private boolean configsLoaded = false;
+
+  /**
+   * Load in the configuration files to know which columns to keep / remove.
+   */
+  private void loadConfigs() {
+
+    // get DB / table names from the environment variables
+    try {
+      databaseName = System.getenv(DB_NAME_ENV);
+      tableName = System.getenv(TABLE_NAME_ENV);
+    } catch (Exception e) {
+      // there should be environment variables
+      e.printStackTrace();
+      System.exit(0);
+    }
+
+    // load in config files
+    JSONParser parser = new JSONParser();
+    JSONObject allWhitelistObject = null;
+    JSONObject databaseBlacklistObject = null;
+    try {
+      allWhitelistObject = (JSONObject) parser.parse(new FileReader(WHITELIST_CONFIG_PATH));
+      databaseBlacklistObject = (JSONObject) parser.parse(new FileReader(BLACKLIST_CONFIG_PATH));
+    } catch (Exception e) {
+      // there should be config files
+      e.printStackTrace();
+      System.exit(0);
+    }
+
+    // get whitelist list for the table
+    try {
+      JSONObject databaseWhitelistObject = (JSONObject) allWhitelistObject.get(databaseName);
+      whitelistColumnNames = new HashSet<String>((List<String>)databaseWhitelistObject.get(tableName));
+      for (String hudiColumn : HUDI_COLUMNS) {
+        whitelistColumnNames.add(hudiColumn);
+      }
+    } catch (Exception e) {
+      // if there is no entry, ignore it
+      whitelistColumnNames = null;
+    }
+
+    // get blacklist list for all tables
+    try {
+      blacklistColumnNames = new HashSet<String>((List<String>)databaseBlacklistObject.get(BLACKLIST_ENTRY));
+    } catch (Exception e) {
+      // there should be a blacklist list
+      e.printStackTrace();
+      System.exit(0);
+    }
+
+  }
+
   @Override
   public Dataset<Row> apply(JavaSparkContext jsc, SparkSession sparkSession, Dataset<Row> rowDataset,
       TypedProperties properties) {
 
+    // load config files (table / db name, whitelisted / blacklisted column names)
+    if (!configsLoaded) {
+      loadConfigs();
+      configsLoaded = true;
+    }
+
+    // add Op column
     Option<String> opColumnOpt = Option.fromJavaOptional(
         Arrays.stream(rowDataset.columns()).filter(c -> c.equals(CustomAWSDmsAvroPayload.OP_FIELD)).findFirst());
     if (!opColumnOpt.isPresent()) {
       rowDataset = rowDataset.withColumn(CustomAWSDmsAvroPayload.OP_FIELD, lit(""));
     }
 
-    // TODO: remove columns with names associated with PII columns
+    // Remove all columns that are always PII
+    for (String name : blacklistColumnNames) {
+      rowDataset = rowDataset.drop(name);
+    }
+
+    // make sure all remaining columns are whitelisted
+    // (if an entry for this table existed)
+    if (whitelistColumnNames != null) {
+      String[] allColumnNames = rowDataset.columns();
+      for (String name : allColumnNames) {
+        if (!whitelistColumnNames.contains(name)) {
+          rowDataset = rowDataset.drop(name);
+        }
+      }
+    }
 
     return rowDataset;
-
   }
 }
